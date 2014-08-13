7c50aeb74159731402f243d64c27469db93142fa
lights li <lightsever@hotmail.com>
2014-08-13 22:30:19 +0800

    0

---
+++ unity/TestBlock/Assets/CreateBox.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Texture2D tex = Resources.Load("j1") as Texture2D;
+
+        for (int y = 0; y < 50;y++ )
+            for (int x = 0; x < 100; x++)
+            {
+                GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                obj.transform.position = new Vector3(x, y, 0);
+                Material mat = new Material(Shader.Find("Diffuse"));
+                mat.mainTexture = tex;
+                mat.mainTextureScale = new Vector2(0.25f, 0.25f);
+                obj.GetComponent<MeshRenderer>().sharedMaterial = mat;
+            }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ unity/TestBlock/Assets/PixelWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+using UnityEngine;
+
+public class PixelWorld:MonoBehaviour
+{
+    public int width =1024;
+    public int height =1024;
+
+    void Start()
+    {
+        wTex = new Texture2D(width, height, TextureFormat.ARGB32, false, false);
+        wordData = new worldData[width, height]; 
+    }
+    public class worldData
+    {
+
+    }
+    worldData[,] wordData
+    {
+        get;
+        private set;
+    }
+    public Texture2D wTex
+    {
+        get;
+        private set;
+    }
+}
+
