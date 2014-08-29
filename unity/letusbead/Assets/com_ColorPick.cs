444244e1d6fb681ee0badb4f637f47b2a2d98bf7
lightszero <lightsever@hotmail.com>
2014-08-29 22:20:14 +0800

    t

--- unity/letusbead/Assets/com_ColorPick.cs
+++ unity/letusbead/Assets/com_ColorPick.cs
@@ -7,18 +7,96 @@ public class com_ColorPick : MonoBehaviour
     public Color useColor = Color.white;
     public Transform PickPanel;
     // Use this for initialization
+
     void Start()
     {
         if (PickPanel == null) PickPanel = this.transform;
-        var rC = PickPanel.Find("ColorPick").GetComponent<UnityEngine.UI.RawImage>();
-        rC.color = useColor;
+        var rC = PickPanel.Find("m/ColorPick").GetComponent<UnityEngine.UI.RawImage>();
+        Texture2D picktex;
+        picktex = new Texture2D(2, 2, TextureFormat.ARGB32, false, true);
+        //picktex.wrapMode = TextureWrapMode.Clamp;
+        rC.texture = picktex;
+        picktex.SetPixel(0, 0, Color.black);
+        picktex.SetPixel(1, 0, Color.black);
+        picktex.SetPixel(0, 1, Color.white);
+        picktex.SetPixel(1, 1, useColor);
+        picktex.Apply();
+
+        var rC2 = PickPanel.Find("mc/ColorPick").GetComponent<UnityEngine.UI.RawImage>();
+        Texture2D pickColor;
+        pickColor = new Texture2D(2, 7, TextureFormat.ARGB32, false, true);
+        pickColor.wrapMode = TextureWrapMode.Repeat;
+        pickColor.SetPixel(0, 0, new Color(1, 0, 0));
+        pickColor.SetPixel(0, 1, new Color(1, 1, 0));
+        pickColor.SetPixel(0, 2, new Color(0, 1, 0));
+        pickColor.SetPixel(0, 3, new Color(0, 1, 1));
+        pickColor.SetPixel(0, 4, new Color(0, 0, 1));
+        pickColor.SetPixel(0, 5, new Color(1, 0, 1));
+        pickColor.SetPixel(0, 6, new Color(1, 0, 0));
+        pickColor.SetPixel(1, 0, new Color(1, 0, 0));
+        pickColor.SetPixel(1, 1, new Color(1, 1, 0));
+        pickColor.SetPixel(1, 2, new Color(0, 1, 0));
+        pickColor.SetPixel(1, 3, new Color(0, 1, 1));
+        pickColor.SetPixel(1, 4, new Color(0, 0, 1));
+        pickColor.SetPixel(1, 5, new Color(1, 0, 1));
+        pickColor.SetPixel(1, 6, new Color(1, 0, 0));
+        pickColor.Apply();
+        rC2.texture = pickColor;
+        //rC.color = useColor;
+        {
+            var ssC = PickPanel.Find("SliderSC").GetComponent<UnityEngine.UI.Slider>();
+            UnityAction<float> onSCChange = (v) =>
+            {
+                if(v<1.0f)
+                {
+                    useColor.r = 1;
+                    useColor.g = Mathf.Lerp(0, 1, v);
+                    useColor.b = 0;
+                    
+                }
+                else if(v<2.0f)
+                {
+                    useColor.r = Mathf.Lerp(1, 0, v-1);
+                    useColor.g = 1;
+                    useColor.b = 0;
+                }
+                else if (v < 3.0f)
+                {
+                    useColor.r = 0;
+                    useColor.g = 1;
+                    useColor.b = Mathf.Lerp(0,1,v-2);
+                }
+                else if (v < 4.0f)
+                {
+                    useColor.r = 0;
+                    useColor.g = Mathf.Lerp(1, 0, v - 3);
+                    useColor.b = 1;
+                }
+                else if (v < 5.0f)
+                {
+                    useColor.r = Mathf.Lerp(0, 1, v - 4);
+                    useColor.g = 0;
+                    useColor.b = 1;
+                }
+                else if (v < 6.0f)
+                {
+                    useColor.r = 1;
+                    useColor.g = 0;
+                    useColor.b = Mathf.Lerp(1, 0, v - 5);
+                }
+                picktex.SetPixel(1, 1, useColor);
+                picktex.Apply();
+            };
+            ssC.onValueChanged.AddListener(onSCChange);
+        }
         {
             var sR = PickPanel.Find("SliderR").GetComponent<UnityEngine.UI.Slider>();
             UnityAction<float> onRChange = (v) =>
                 {
 
                     useColor.r = v;
-                    rC.color = useColor;
+                    picktex.SetPixel(1, 1, useColor);
+                    picktex.Apply();
                 };
             sR.onValueChanged.AddListener(onRChange);
         }
@@ -28,7 +106,8 @@ public class com_ColorPick : MonoBehaviour
             {
 
                 useColor.g = v;
-                rC.color = useColor;
+                picktex.SetPixel(1, 1, useColor);
+                picktex.Apply();
             };
             sG.onValueChanged.AddListener(onGChange);
         }
@@ -38,7 +117,8 @@ public class com_ColorPick : MonoBehaviour
             {
 
                 useColor.b = v;
-                rC.color = useColor;
+                picktex.SetPixel(1, 1, useColor);
+                picktex.Apply();
             };
             sB.onValueChanged.AddListener(onBChange);
         }
---
+++ unity/letusbead/Assets/openbtn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openbtn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
+            {
+                Application.OpenURL("file://d:/");
+            });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
