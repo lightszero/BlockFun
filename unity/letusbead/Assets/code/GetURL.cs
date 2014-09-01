2f3f58fbd4acd304fa146168368102eb84108779
lightszero <lightsever@hotmail.com>
2014-09-01 17:13:13 +0800

    t

---
+++ unity/letusbead/Assets/code/GetURL.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GetURL : MonoBehaviour
+{
+    public com_pixelEdit edit;
+    // Use this for
+    public UnityEngine.UI.InputField input;
+    public static string fullUrl;
+    void Start() {
+        Application.ExternalEval("u.getUnity().SendMessage(\"" + name + "\", \"ReceiveURL\", document.URL);");
+    }
+ 
+    public void ReceiveURL(string url) {
+        // this will include the full URL, including url parameters etc.
+        fullUrl = url;
+        int i = fullUrl.IndexOf('#');
+        if(i<=0)return;
+
+        input.value = fullUrl.Substring(i + 1);
+        fullUrl = fullUrl.Substring(0, i);
+        savebtn.ReadByte(input.value, edit.edit, edit.palette);
+    }
+}
--- unity/letusbead/Assets/code/savebtn.cs
+++ unity/letusbead/Assets/code/savebtn.cs
@@ -1,44 +1,105 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class savebtn : MonoBehaviour
 {
 
     public com_pixelEdit edit;
     // Use this for
-
+    public UnityEngine.UI.InputField input;
     void Start() {
         this.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
            {
-               byte[] palette = edit.palette.EncodeToPNG();
-               byte[] index = edit.edit.EncodeToPNG();
+               List<byte> bytes = new List<byte>();
                var pc = edit.palette.GetPixels32(0);
                var ic = edit.edit.GetPixels32(0);
-               Color32[] colordata = new Color32[edit.edit.width * edit.edit.height];
+               Dictionary<byte, Color32> usep = new Dictionary<byte, Color32>();
+               bytes.Add((byte)edit.edit.width);
+               bytes.Add((byte)edit.edit.height);
                for (int i = 0; i < ic.Length; i++)
                {
-                   colordata[i] = pc[ic[i].a];
+                   byte ind= ic[i].a;
+                   bytes.Add(ind);
+                   if(ind>0)
+                   {
+                       if(usep.ContainsKey(ind)==false)
+                       {
+                           usep.Add(ind,pc[ind]);
+                       }
+                   }
                }
-               Texture2D tex = new Texture2D(edit.edit.width, edit.edit.height, TextureFormat.ARGB32, false);
-               tex.SetPixels32(colordata);
-               tex.Apply();
-               var color = tex.EncodeToPNG();
-               Object.Destroy(tex);
-               using (System.IO.Stream s = System.IO.File.Create(Application.persistentDataPath + "/temp_c.png"))
+               bytes.Add((byte)usep.Count);
+               foreach(var up in usep)
                {
-                   s.Write(color, 0, color.Length);
+                   bytes.Add(up.Key);
+                   bytes.Add(up.Value.r);
+                   bytes.Add(up.Value.g);
+                   bytes.Add(up.Value.b);
                }
-               using (System.IO.Stream s = System.IO.File.Create(Application.persistentDataPath + "/temp_p.png"))
-               {
-                   s.Write(palette, 0, palette.Length);
-               }
-               using (System.IO.Stream s = System.IO.File.Create(Application.persistentDataPath + "/temp_i.png"))
-               {
-                   s.Write(index, 0, index.Length);
-               }
-           });
-	}
 
+               var s =           LZMAHelper.Compress(new System.IO.MemoryStream(bytes.ToArray()), (uint)bytes.Count);
+               byte[] nb = new byte[s.Length];
+               s.Read(nb, 0, nb.Length);
+               string str = System.Convert.ToBase64String(nb);
+               string str2 = System.Uri.EscapeDataString(str);
+               input.value = str2;
+               Debug.Log(input.value.Length);
+           });
+        input.onSubmit.AddListener((str)=>
+        {
+            ReadByte(str, edit.edit, edit.palette);
+
+        });
+	}
+    public static void ReadByte(string scode,Texture2D src,Texture2D p)
+    {
+        try
+        {
+            string strbase64 = System.Uri.UnescapeDataString(scode);
+            byte[] bb = System.Convert.FromBase64String(strbase64);
+            var s = LZMAHelper.DeCompress(new System.IO.MemoryStream(bb), (uint)bb.Length);
+            bb = new byte[s.Length];
+            s.Read(bb, 0, bb.Length);
+
+            int seek = 0;
+            int width = bb[seek]; seek++;
+            if (width == 0) width = 256;
+            int height = bb[seek]; seek++;
+            if (height == 0) height = 256;
+            Debug.Log("w=" + width + ",h=" + height);
+            Color32[] pp = src.GetPixels32(0);
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    byte ind = bb[seek]; seek++;
+                    pp[y * width + x].a = ind;
+                }
+            }
+            src.SetPixels32(pp, 0);
+            src.Apply();
+            Color32[] ppp = p.GetPixels32(0);
+            int c = bb[seek]; seek++;
+            for (int i = 0; i < c; i++)
+            {
+                byte inde = bb[seek]; seek++;
+                byte r = bb[seek]; seek++;
+                byte g = bb[seek]; seek++;
+                byte b = bb[seek]; seek++;
+                ppp[inde].r = r;
+                ppp[inde].g = g;
+                ppp[inde].b = b;
+
+            }
+            p.SetPixels32(ppp);
+            p.Apply();
+        }
+        catch
+        {
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
