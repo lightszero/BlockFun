5b8582f3d1eeb545482ac02d18d0ff584e03f71c
lightszero <lightsever@hotmail.com>
2014-12-26 20:52:34 +0800

    t

---
+++ unity/focus/Assets/fillfull.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class fillfull : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localScale = new Vector3((float)Screen.width / (float)Screen.height, 1, 0);
+	}
+}
---
+++ unity/focus/Assets/moveuv.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveuv : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        mainmat = this.GetComponent<MeshRenderer>().material;
+    }
+    Material mainmat;
+
+    // Update is called once per frame
+    float size = 0.1f;
+    void Update()
+    {
+        Vector3 pos = Input.mousePosition;
+        size += Input.mouseScrollDelta.y / 100.0f;
+        if (size < 0.1f) size = 0.1f;
+        if (size > 0.5f) size = 0.5f;
+        mainmat.mainTextureScale = new Vector2(1 / size, 1 / size);
+        float xmax = 1 / size;
+        float ymax = 1 / size;
+        float x = 0.5f - pos.x / Screen.width * xmax;
+        float y = 0.5f - pos.y / Screen.height * ymax;
+        mainmat.mainTextureOffset = new Vector2(x, y);
+    }
+}
